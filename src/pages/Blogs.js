1a3dfa3824c738dcfee8eefb775699959412abb4
Keketso Matsuma <49682202+MatsumaKeketso@users.noreply.github.com>
2022-02-16 21:49:46 +0200

    Layout

--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import { cardWrapper } from './Blogs.module.scss'
 import NewsItem from '../components/NewsCard/NewsItem'
 import Stack from '@mui/material/Stack';
